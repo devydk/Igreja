3afaf7c461cdac42709d4338de59aadad39d49ef
devydk <devyd.santana@hotmail.com>
2021-05-03 22:18:55 -0300

    mudei o arquivo MainStack.js

--- Downloads/AwesomeProject/src/stacks/MainStack.js
+++ Downloads/AwesomeProject/src/stacks/MainStack.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Preload from '../screens/Preload';
-import SignIn from '../screens/SignIn';
-import SignUp from '../screens/SignUp';
+import SignIn from '../screens/SignIn/index';
+import SignUp from '../screens/SignUp/index';
 
 const Stack = createStackNavigator();
 
